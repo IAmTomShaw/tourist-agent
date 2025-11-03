b4dccafce5c3bbd00405a18ec7e25665c6272ad1
Tom Shaw <73386292+IAmTomShaw@users.noreply.github.com>
2025-11-03 19:47:48 +0000

    Clarify activity date constraints in agent prompt
    The prompt was sometimes returning suggestions outside of the defined trip period. This update to the prompt appears to have improved the quality of the results :)

--- src/agent.py
+++ src/agent.py
@@ -35,6 +35,8 @@ async def run_agent(start_date: str, end_date: str, location: str, busy_periods:
 
       Do not return multiple activities for the same time slot. Try to fill all of the available time slots with unique activities. Make sure that the activities you suggest do not overlap with any of the busy periods provided.
 
+      Do not return suggestions that are not happening between the start_date and end_date provided.
+
       If the user's existing calendar events are in locations far from the trip location, take into account that travel time will be needed to get to/from the activity between calendar events.
 
       If the price of an activity is unknown, provide an estimated price range.
